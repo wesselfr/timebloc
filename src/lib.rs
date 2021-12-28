c7356be0c2a71230b84078fa1b487237f1f0b66f
Wessel Frijters <wessel.frijters@gmail.com>
2021-12-28 15:51:36 +0100

    Added config loading. Added default config as backup in case a config doesn't exsist

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 extern crate colored;
-use std::fs::File;
-use std::io::{self, Write};
+use std::fs::{File};
+use std::io::{Write, Read};
 use std::{env, fs::create_dir};
 
-use chrono::{self, DateTime, Local, Timelike};
+use chrono::{self, DateTime, Local, Timelike, Datelike};
 use colored::*;
-use serde::{ser, Deserialize, Serialize};
-use serde_json::{Error, Result};
+use serde::{Deserialize, Serialize};
+use serde_json::{Result};
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
@@ -18,9 +18,6 @@ pub struct Config {
 pub fn save_config(config: &Config) -> Result<()> {
     // Serialize it to a JSON string.
     let j = serde_json::to_string(config)?;
-
-    // Print, write to a file, or send to an HTTP server.
-    println!("{}", j);
 
     let mut directory = env::temp_dir();
     directory.push("timebloc");
@@ -42,8 +39,89 @@ pub fn save_config(config: &Config) -> Result<()> {
     Ok(())
 }
 
-#[derive(Serialize, Deserialize, Copy, Clone)]
+fn load_config_from_string(contents: &String) -> Config{
+    let config: Config = serde_json::from_str(&contents).expect("Error while loading config.");
+    config
+}
+
+pub fn load_config() -> Result<Config>{
+    let config: Config;
+
+    let mut directory = env::temp_dir();
+    directory.push("timebloc");
+
+    let file_path = directory.join("config.json");
+
+    let file = File::open(file_path);
+    if file.is_ok(){
+        let mut contents = String::new();
+        file.unwrap().read_to_string(&mut contents).unwrap();
+        config = load_config_from_string(&contents);
+    }
+    else{
+        // Create and save default config.
+        config = Config {
+            visible_hours_start: 8,
+            visible_hours_end: 17,
+            visible_hours_subdivision: 4,
+        };
+        save_config(&config).expect("Error while saving config.");
+    }
+    Ok(config)
+}
+
+pub fn save_appointments(appointments: &Vec<TimeBlock>) -> Result<()>{
+    let j = serde_json::to_string(&appointments)?;
+
+    let mut directory = env::temp_dir();
+    directory.push("timebloc");
+
+    // Create directory if it doesn't exsist.
+    if !directory.is_dir() {
+        let status = create_dir(&directory);
+        if status.is_err() {
+            panic!("Could not create directory..")
+        }
+    }
+
+    println!("Saving appointments to: {}", directory.to_str().unwrap());
+    let temp_file = directory.join("appointments.json");
+
+    let mut file = File::create(temp_file).unwrap();
+    writeln!(&mut file, "{}", j);
+
+    Ok(())
+}
+
+fn load_appointments_from_string(data: &String) -> Result<Vec<TimeBlock>> {
+    let appointments = serde_json::from_str(data);
+    appointments
+}
+
+pub fn load_appointments() -> Vec<TimeBlock>{
+    let mut appointments: Vec<TimeBlock> = Vec::new();
+    let mut directory = env::temp_dir();
+    directory.push("timebloc");
+
+    if !directory.is_dir(){
+       return appointments;
+    }
+
+    let file_path = directory.join("appointments.json");
+
+    let mut file = File::open(file_path).expect("Unable to open the file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Unable to read the file");
+    
+    appointments = load_appointments_from_string(&contents).expect("Unable to load appointments");
+    return appointments;
+}
+
+
+#[derive(Serialize, Deserialize, Clone)]
 pub struct TimeBlock {
+    pub name: String,
+    pub date: u32, // YYYYMMDD
     pub start_hour: u8,
     pub end_hour: u8,
 }
@@ -51,16 +129,24 @@ pub struct TimeBlock {
 impl TimeBlock {
     pub fn new(start: u8, end: u8) -> TimeBlock {
         TimeBlock {
+            name: "New timebloc".to_string(),
+            date: get_date_formatted(&chrono::offset::Local::now()),
             start_hour: start,
             end_hour: end,
         }
     }
-    pub fn within_timeblock(self, i: u8) -> bool {
+    pub fn within_timeblock(&self, i: u8) -> bool {
         if i >= self.start_hour && i < self.end_hour {
             return true;
         }
         false
     }
+}
+
+// Returns the date formatted as YYYY MM DD
+pub fn get_date_formatted(date: &DateTime<Local>) -> u32{
+    let format: u32 = date.year() as u32 * 10000 + date.month() * 100 + date.day();
+    format
 }
 
 pub fn print_timeslot(hour: u8, is_free: bool, subdivision: u32, current: &DateTime<Local>) {
--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,51 @@ use chrono::{self};
 use timebloc::*;
 
 fn main() {
-    let config = Config {
-        visible_hours_start: 8,
-        visible_hours_end: 17,
-        visible_hours_subdivision: 4,
-    };
 
-    save_config(&config);
+    let config_load_result = load_config();
+    let config: Config;
+
+    // Check if config is valid, if not create a new one.
+    match config_load_result {
+        Ok(val) => {
+            config = val;
+        },
+        Err(_err) => {
+            config = Config {
+                visible_hours_start: 8,
+                visible_hours_end: 17,
+                visible_hours_subdivision: 4,
+            };
+            save_config(&config).expect("Error while saving config.");
+        }
+    }
+
+    // let config = Config {
+    //     visible_hours_start: 8,
+    //     visible_hours_end: 17,
+    //     visible_hours_subdivision: 4,
+    // };
+
+    // save_config(&config);
 
     assert!(config.visible_hours_end <= 24);
     assert!(config.visible_hours_start < config.visible_hours_end);
 
-    let appointments = [
-        TimeBlock::new(10, 12),
-        TimeBlock::new(13, 16),
-        TimeBlock::new(23, 24),
-    ];
+    let mut appointments = load_appointments();
+
+    // Fill with test data if needed.
+    if appointments.len() == 0{
+        appointments.push(TimeBlock::new(10, 12));
+        appointments.push(TimeBlock::new(13, 16));
+        appointments.push(TimeBlock::new(23, 24));
+        save_appointments(&appointments.to_vec()).expect("Error while saving temp appointments");
+    }
 
     let current_time = chrono::offset::Local::now();
 
     for i in config.visible_hours_start..config.visible_hours_end {
         let mut is_free = true;
-        for timeblock in appointments {
+        for timeblock in &appointments {
             if timeblock.within_timeblock(i) {
                 is_free = false
             }
